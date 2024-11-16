38cda490bd068f0f1d2b5b1504cf204a4b83d4f7
AndranikAdyan <andranikadyan@gmail.com>
2024-11-16 20:46:41 +0400

    clean up

--- bonus/monitoring.sh
+++ bonus/monitoring.sh
@@ -6,9 +6,6 @@ failed_logs=$(tail -n15 /var/log/auth.log | grep "Connection closed by authentic
 
 #touch -c ./.logs
 
-#if [ $failed_logs -ge 3 ] && [ ! "$(echo "$all_logs")" = "$(cat ./.logs)" ]; then
 if [ $failed_logs -ge 3 ]; then
 	echo -e "Subject: Warning!\nSomeone is attempting to log in to the server." | msmtp andranikadyan@gmail.com
 fi
-
-#echo "$all_logs" > ./.logs
